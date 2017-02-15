2a61eddb565e296d6b78b2bf05f364b46af62c32
cofl <cllacour@mtu.edu>
2017-02-15 11:03:38 -0500

    Added icons

--- src/main/java/aa/monoglot/Monoglot.java
+++ src/main/java/aa/monoglot/Monoglot.java
@@ -7,9 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ResourceBundle;
@@ -31,28 +33,11 @@ public class Monoglot extends Application {
         try {
             bundle = ResourceBundle.getBundle("lang/lang");
         } catch(Exception e){
-            Alert fatalError = new Alert(Alert.AlertType.ERROR);
-            fatalError.setTitle("Resource Load Error");
-            fatalError.setHeaderText("An error occurred while loading the application localization file.");
-            fatalError.setContentText("Unless you're doing something funky, please send this to the developer," +
-                    " along with an explanation of what you were doing:");
-
-            StringWriter stringWriter = new StringWriter();
-            e.printStackTrace(new PrintWriter(stringWriter));
-            TextArea area = new TextArea(stringWriter.toString());
-            area.setEditable(false);
-            area.setWrapText(true);
-
-            fatalError.getDialogPane().setExpandableContent(area);
-            fatalError.showAndWait();
+            showError(e, null);
             Platform.exit();
         }
 
         try {
-             // comment this line to enable error test
-            /*if(Math.random() < Double.POSITIVE_INFINITY)
-                throw new Exception();
-            // */
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/app.fxml"), bundle);
             AnchorPane p = loader.load();
             mainController = loader.getController();
@@ -63,30 +48,52 @@ public class Monoglot extends Application {
             window.minHeightProperty().bind(p.minHeightProperty());
             window.minWidthProperty().bind(p.minWidthProperty());
         } catch(Exception e){
-            Alert fatalError = new Alert(Alert.AlertType.ERROR);
-            fatalError.setTitle(bundle.getString("dialog.error.fatalError.title"));
-            fatalError.setHeaderText(bundle.getString("dialog.error.fatalError.header"));
-            fatalError.setContentText(bundle.getString("dialog.error.fatalError.text"));
-
-            StringWriter stringWriter = new StringWriter();
-            e.printStackTrace(new PrintWriter(stringWriter));
-            TextArea area = new TextArea(stringWriter.toString());
-            area.setEditable(false);
-            area.setWrapText(true);
-
-            fatalError.getDialogPane().setExpandableContent(area);
-            fatalError.showAndWait();
+            showError(e, true);
             Platform.exit();
+        }
+
+        try {
+            InputStream image = getClass().getClassLoader().getResourceAsStream("images/logo.png");
+            primaryStage.getIcons().addAll(
+                    new Image(image),
+                    new Image(image, 128, 128, true, true),
+                    new Image(image, 64, 64, true, true),
+                    new Image(image, 32, 32, true, true),
+                    new Image(image, 16, 16, true, true)
+            );
+        } catch(Exception e){
+            // fail silently, this part isn't vital.
         }
 
         window.show();
     }
 
-    public void showError(Exception e) {
+    public void showError(Exception e){
+        showError(e, false);
+    }
+
+    public void showError(Exception e, Boolean isFatal) {
         Alert error = new Alert(Alert.AlertType.ERROR);
-        error.setTitle(bundle.getString("dialog.error.normal.title"));
-        error.setHeaderText(bundle.getString("dialog.error.normal.header"));
-        error.setContentText(bundle.getString("dialog.error.fatalError.text"));
+
+        String title, header, text;
+        if(isFatal == null) {
+            title = "Resource Load Error";
+            header = "An error occurred while loading the application localization file.";
+            text = "Unless you're doing something funky, please send this to the developer," +
+                    " along with an explanation of what you were doing:";
+        } else if(isFatal){
+            title = bundle.getString("dialog.error.fatalError.title");
+            header = bundle.getString("dialog.error.fatalError.header");
+            text = bundle.getString("dialog.error.fatalError.text");
+        } else {
+            title = bundle.getString("dialog.error.normal.title");
+            header = bundle.getString("dialog.error.normal.header");
+            text = bundle.getString("dialog.error.normal.text");
+        }
+
+        error.setTitle(title);
+        error.setHeaderText(header);
+        error.setContentText(text);
 
         StringWriter stringWriter = new StringWriter();
         e.printStackTrace(new PrintWriter(stringWriter));
