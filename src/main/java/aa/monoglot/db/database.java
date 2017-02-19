88fa7c5f2a9ea1e33bec70b3656d589c3a0fcff1
amstanag <amstanag@mtu.edu>
2017-02-19 14:52:06 -0500

    Added code to create a connection to a database

--- src/main/java/aa/monoglot/db/database.java
+++ src/main/java/aa/monoglot/db/database.java
@@ -1,12 +1,26 @@
 package aa.monoglot.db;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Created by Alex on 2/18/17.
  */
 public class database {
 
-    public static void main(String...args){
-        
+    public static void main(String...args) throws ClassNotFoundException {
+            try {
+                Class.forName("org.h2.Driver");
+                Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+                        //application code here
+                        conn.close();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-}
+
