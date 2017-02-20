eb4bd50ae3381f628a8591e67d61c3b4fd4cc6cd
amstanag <amstanag@mtu.edu>
2017-02-20 11:41:57 -0500

    changed name........

--- src/main/java/aa/monoglot/db/Database.java
+++ src/main/java/aa/monoglot/db/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 /**
  * Created by Alex on 2/18/17.
  */
-public class database {
+public class Database {
 
     public static void main(String...args) throws ClassNotFoundException {
             try {
