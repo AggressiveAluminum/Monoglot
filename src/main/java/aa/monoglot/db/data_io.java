5a37a1fd692ec178a2b05dd6cc7067f78328fff1
Matthew C Wenzel <mcwenzel@mtu.edu>
2017-02-20 11:46:50 -0500

    Added a java class that handles the data io for the files and the database. Just wrote skeleton code.

---
+++ src/main/java/aa/monoglot/db/data_io.java
@@ -0,0 +1,25 @@
+package aa.monoglot.db;
+
+/**
+ * Created by Matt on 2/20/17.
+ */
+public class data_io {
+
+    boolean paused = false;
+
+    public void pauseDB(){
+
+    }
+
+    public void save(){
+
+    }
+
+    public void resumeDB(){
+
+    }
+
+    public void doSQLAction(String action){
+
+    }
+}
