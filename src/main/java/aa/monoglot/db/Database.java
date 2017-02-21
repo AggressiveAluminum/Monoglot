48979104a34da5d1420be39d6cfa2ce82b24e624
amstanag <amstanag@mtu.edu>
2017-02-20 19:38:37 -0500

    Changing where the database is created and held

--- src/main/java/aa/monoglot/db/Database.java
+++ src/main/java/aa/monoglot/db/Database.java
@@ -1,6 +1,7 @@
 package aa.monoglot.db;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,11 +10,10 @@ import java.sql.SQLException;
  * Created by Alex on 2/18/17.
  */
 public class Database {
-
     public static void main(String... args) throws ClassNotFoundException {
         try {
             Class.forName("org.h2.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            Connection conn = DriverManager.getConnection(getConnString(Paths.get("~/Documents")));
             //application code here
             conn.close();
         } catch (ClassNotFoundException e) {
@@ -23,12 +23,12 @@ public class Database {
         }
     }
 
-    private String getConnString(Path dir) {
-        //creating a path string for connection
-        String thing = dir.toString();
-        thing.replaceAll("\\\\", "/");
-        thing = thing + "/db";
-        return "jdbc:h2:file:" + thing;
+    /**
+     * creating a path string for connection
+     * @param dir
+     */
+    private static String getConnString(Path dir) {
+        return "jdbc:h2:file:" + dir.toAbsolutePath().toString().replaceAll("\\\\", "/") + "/db";
     }
 }
 
