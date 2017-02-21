e0179858f1c318f671d6efa890a5d5eeb55e3474
Matthew C Wenzel <mcwenzel@mtu.edu>
2017-02-20 19:10:41 -0500

    Fleshed out the methods for the data_io method. There is a method to run a query, pause, open, and close the db.

--- src/main/java/aa/monoglot/db/data_io.java
+++ src/main/java/aa/monoglot/db/data_io.java
@@ -1,25 +1,72 @@
 package aa.monoglot.db;
 
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayDeque;
+import java.util.Properties;
+
 /**
  * Created by Matt on 2/20/17.
  */
 public class data_io {
 
-    boolean paused = false;
+    boolean paused;
+    private ArrayDeque queue;
+    Connection conn = null;
 
-    public void pauseDB(){
-
+    public data_io() {
+        queue = new ArrayDeque();
+        paused = false;
     }
 
-    public void save(){
+    public Connection dbOpenConn(){
+        Connection conn = null;
+        try {
+            Class.forName("org.h2.Driver");
+             conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void dbCloseConn(Connection conn){
+        try {
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pauseDB(){
+        if ( conn != null{
+            dbCloseConn(conn);
+        }
     }
 
     public void resumeDB(){
-
+        conn = dbOpenConn();
     }
 
-    public void doSQLAction(String action){
+    private ResultSet doSQLAction(String action){
 
+        Connection conn = dbOpenConn();
+        queue.add(action);
+        String query = queue.pop().toString();
+        ResultSet results = null;
+        try{
+            conn.setAutoCommit(false);
+            PreparedStatement preparedQuerey = conn.prepareStatement(query);
+            results = preparedQuerey.executeQuery();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return results;
     }
 }
