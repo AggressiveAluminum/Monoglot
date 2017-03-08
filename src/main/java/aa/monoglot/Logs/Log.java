ff7296ed7275b5bb35e16c07bd904a6d358d4e83
Matthew C Wenzel <mcwenzel@mtu.edu>
2017-03-08 13:45:11 -0500

    Added more methods to Log API. Have to figure out how to get the logs to save to a file.

--- src/main/java/aa/monoglot/Logs/Log.java
+++ src/main/java/aa/monoglot/Logs/Log.java
@@ -12,12 +12,52 @@ public class Log {
 
 
     public Log (String className){
-        String loggerClassnameArguement = className + ".class.getName()";
+        String loggerClassnameArgument = className.getClass().toString();
 
-        logger = Logger.getLogger(loggerClassnameArguement);
+        logger = Logger.getLogger(loggerClassnameArgument);
     }
 
     public Logger getLogger(){
+
         return logger;
+    }
+
+    public void logIssueSevere(String message){
+        LogRecord record = new LogRecord(Level.SEVERE, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logIssueWarning(String message){
+        LogRecord record = new LogRecord(Level.WARNING, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logInfo(String message){
+        LogRecord record = new LogRecord(Level.INFO, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logIssueFine(String message){
+        LogRecord record = new LogRecord(Level.FINE, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logIssueFiner(String message){
+        LogRecord record = new LogRecord(Level.FINER, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logIssueFinest(String message){
+        LogRecord record = new LogRecord(Level.FINEST, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public void logIssueAll(String message){
+        LogRecord record = new LogRecord(Level.ALL, message);
+        record.setLoggerName(logger.getName());
+    }
+
+    public String printLog(){
+        return null;
     }
 }
