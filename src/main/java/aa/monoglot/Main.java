92b7ee851d21d23357f567e9a3d8b1bbcb7be121
cofl <cllacour@mtu.edu>
2017-03-20 08:58:00 -0400

    Integrated logging, doomsday.

--- src/main/java/aa/monoglot/Main.java
+++ src/main/java/aa/monoglot/Main.java
@@ -1,9 +1,12 @@
 package aa.monoglot;
 
+import aa.monoglot.Logs.Log;
 import aa.monoglot.util.OS;
 import javafx.application.Application;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @see Monoglot
@@ -11,6 +14,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         OS.verify();
+        Log.loggerInit(OS.SETTINGS_DIRECTORY.resolve("log-" + DateTimeFormatter.ISO_INSTANT.format(LocalDateTime.now()) + ".log"));
         Application.launch(Monoglot.class, args);
     }
 }
