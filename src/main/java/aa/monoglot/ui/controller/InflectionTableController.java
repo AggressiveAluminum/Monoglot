8872a2b628d3e7204be20e7f456598ffcbce55c2
amstanag <amstanag@mtu.edu>
2017-03-24 11:54:54 -0400

    re-added files

---
+++ src/main/java/aa/monoglot/ui/controller/InflectionTableController.java
@@ -0,0 +1,17 @@
+package aa.monoglot.ui.controller;
+
+/**
+ * Created by Alex on 3/3/17.
+ */
+public class InflectionTableController extends AbstractChildController<MonoglotController> {
+
+    @Override
+    protected void postInitialize() {
+        System.out.println("Test");
+    }
+
+    @Override
+    protected void clearInfo() {
+
+    }
+}
