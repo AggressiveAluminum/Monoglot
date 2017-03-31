a3de8182928b8b60e256be83c5241ab7bf0d0da1
amstanag <amstanag@mtu.edu>
2017-03-31 11:06:16 -0400

    Adding button listeners in java file

--- src/main/java/aa/monoglot/ui/controller/InflectionTableController.java
+++ src/main/java/aa/monoglot/ui/controller/InflectionTableController.java
@@ -1,14 +1,29 @@
 package aa.monoglot.ui.controller;
 
 import aa.monoglot.ui.ControlledTab;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class InflectionTableController implements GeneralController {
     @FXML
     private ControlledTab tab;
+    @FXML
+    private ComboBox<?> InflectionTable;
 
     @FXML
     private void initialize(){
         tab.controller(this);
+    }
+
+    public void tableSelect(ActionEvent actionEvent) {
+        String itemName = (String)(InflectionTable.getSelectionModel().getSelectedItem());
+
+        if(itemName.equals("Language 1")){
+        System.out.println("Hello");
+        }else{
+            System.out.println("NOPE");
+        }
+
     }
 }
