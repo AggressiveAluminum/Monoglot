4524f8a89aae30a81ee2ff083229251e28a4bb4c
Darren Wiltse <dkwiltse@gmail.com>
2017-04-02 16:40:29 -0400

    Types complete, categories underway.

--- src/main/java/aa/monoglot/db/Categories.java
+++ src/main/java/aa/monoglot/db/Categories.java
@@ -1,4 +1,10 @@
-import java.util.ArrayList;
+package aa.monoglot.db;
+
+import aa.monoglot.project.Project;
+import aa.monoglot.util.UT;
+
+import java.sql.*;
+import java.util.UUID;
 
 /**
  * Created by Darren on 3/13/17.
@@ -7,15 +13,12 @@ public class Categories {
 
     private static class Node {
 
-        public ArrayList<Node> children;
-
         private Node parent;
         private String name;
 
         public Node(String n) {
             parent = null;
             name = n;
-            children = null;
         }
 
         public void setParent(Node p) {
@@ -24,14 +27,6 @@ public class Categories {
 
         public Node getParent() {
             return this.parent;
-        }
-
-        public void addChild(Node c) {
-            children.add(c);
-        }
-
-        public ArrayList<Node> getChildren() {
-            return children;
         }
 
         public void setName(String n) {
@@ -43,13 +38,18 @@ public class Categories {
         }
     }
 
-    public static void add(String n) {
-        Node node = new Node(n);
-    }
+    static final String INSERT_STR = "INSERT INTO tags VALUES (?, ?, ?, ?, ?)";
+    static final String UPDATE_STR = "UPDATE tags SET id=?, name=?, full_name=?, parent_category=?, description=?";
+    static final String SELECT_STR = "SELECT * FROM categories WHERE id = ?";
+
+    private final static int ID_COL = 1, NAME_COL = 2, FNAME_COL = 3, PARCAT_COL = 4, DESC_COL = 5;
+
+    public static  UUID id = null;
+    public static String name = null, full_name = null, parent_category = null, description = null;
 
     @Override
     public String toString() {
-        //tree
+        return parent_category + "<" + name;
     }
 
     @Override
