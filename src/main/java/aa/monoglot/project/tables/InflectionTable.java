5b7463cc363418c56358eb71a53711a96cb5b201
amstanag <amstanag@mtu.edu>
2017-04-10 11:28:45 -0400

    Trying to work on serialization stuff

--- src/main/java/aa/monoglot/project/tables/InflectionTable.java
+++ src/main/java/aa/monoglot/project/tables/InflectionTable.java
@@ -1,5 +1,8 @@
 package aa.monoglot.project.tables;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
@@ -9,7 +12,14 @@ public class InflectionTable implements Serializable {
 
     private void save(){
 
-
+        Object object = null;
+    //Serialize table
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("table.ser"))) {
+            out.writeObject(object);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
